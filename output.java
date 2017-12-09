e9726c6724a3ecb9db2ea6059268ea2f58913e7f
DESKTOP-O90QIGB\agung <agung.aka@gmail.com>
2017-12-09 18:35:14 +0700

    tes2

---
+++ output.java
@@ -0,0 +1,29 @@
+Nilai kondisi (Umur > 32) && (Berat < 70) adalah false
+Nilai kondisi (Umur > 32) && (Berat >= 70) adalah false
+Nilai kondisi (Umur >= 25) && (Berat < 70) adalah false
+Nilai kondisi (Umur >=25) && (Berat >= 70) adalah false
+
+Nilai kondisi (Umur > 32) || (Berat < 70) adalah true
+Nilai kondisi (Umur > 32) || (Berat >= 70) adalah false
+Nilai kondisi (Umur >= 25) || (Berat < 70) adalah true
+Nilai kondisi (Umur >=25) || (Berat >= 70) adalah false
+
+Nilai kondisi (Umur > 32) & (Berat < 70) adalah false
+Nilai kondisi (Umur > 32) & (Berat >= 70) adalah false
+Nilai kondisi (Umur >= 25) & (Berat < 70) adalah false
+Nilai kondisi (Umur >=25) & (Berat >= 70) adalah false
+
+Nilai kondisi (Umur > 32) | (Berat < 70) adalah true
+Nilai kondisi (Umur > 32) | (Berat >= 70) adalah false
+Nilai kondisi (Umur >= 25) | (Berat < 70) adalah true
+Nilai kondisi (Umur >=25) | (Berat >= 70) adalah false
+
+Nilai kondisi (Umur > 32) ^ (Berat < 70) adalah true
+Nilai kondisi (Umur > 32) ^ (Berat >= 70) adalah false
+Nilai kondisi (Umur >= 25) ^ (Berat < 70) adalah true
+Nilai kondisi (Umur >=25) ^ (Berat >= 70) adalah false
+
+Nilai kondisi !(Umur >=25) adalah true
+Nilai kondisi !(Berat < 70) adalah false
+Nilai kondisi !(Berat == 70) adalah true
+Nilai kondisi !(Umur == 30) adalah true
